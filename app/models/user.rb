680abea2153b7b8b73b607ac521358cc4cd4c413
Israel-Laguan <israellaguan@gmail.com>
2019-11-21 16:18:48 -0500

    [feat] Sinup: Add connections to signup
    What?:
    - Changes on routes, User and Session to signup a new user
    Breaking Changes?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    has_secure_password
+  has_secure_password
+  before_create :create_digest
+
+  validates :name, presence: true, length: { in: 6..20 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
+  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
+
+  private
+
+  def create_digest
+    self.remember_token = Digest::SHA1.hexdigest(SecureRandom.urlsafe_base64.to_s)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/delete'
   resource :home, controller: :home, only: [:show]
   root to: 'home#show'
   get 'home/show'
+  resources :sessions, only: [:create, :new, :destroy ]
+  get 'signup' => "users#new"
+  post 'signup' => 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
